568d3fc37ba6b2596bba3a6bd4e95cc6bff8281b
hiroki-inoue-git <weeklyjump0928@gmail.com>
2020-08-22 17:20:23 +0900

    メインプロセスの仮作成

---
+++ app/src/main/java/com/example/drugscity/MainProcess.java
@@ -0,0 +1,45 @@
+package com.example.drugscity;
+
+public class MainProcess {
+
+    private PlayerManagement playerManage;
+    private final int FASE_NUM = 3;
+
+    void mainGameLoop(){
+        /***
+            ゲームのメインループ処理
+         ***/
+        boolean isGameEnd = false;
+        while (!isGameEnd){
+            // 1ターンの処理
+            isGameEnd = doThreeFase();
+            playerManage.checkDrug();
+        }
+    }
+
+    boolean doThreeFase(){
+        /***
+         3フェーズ処理する
+         ***/
+
+        boolean isGameEnd = false;
+        for (int i = 0; i < FASE_NUM; i++){
+            isGameEnd = doOneFase();
+            if (isGameEnd){
+                return isGameEnd;
+            }
+        }
+        // isGameEndは絶対にfalse
+        return isGameEnd;
+    }
+
+    boolean doOneFase(){
+        /***
+         1フェーズの処理
+         ***/
+        playerManage.decideActionSequence();
+        playerManage.decideAllAction();
+        playerManage.doAction();
+        return playerManage.isGameEnd();
+    }
+}
