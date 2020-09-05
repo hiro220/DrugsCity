c9b3582163a773fed379ab5b9cb90f04b79cb72e
totem12 <7s1fma@gmail.com>
2020-09-05 15:35:17 +0900

    直接参照しているところ関数で参照するようにした

--- app/src/main/java/com/example/drugscity/PhaseManagement.java
+++ app/src/main/java/com/example/drugscity/PhaseManagement.java
@@ -16,11 +16,11 @@ public class PhaseManagement {
 
         for(int i = 0; i < playerList.size(); i++) {
             //脱落したプレイヤーだったら飛ばす
-            if(playerList.get(i).dead){
+            if(playerList.get(i).isDead()){
                 continue;
             }
 
-            playerList.get(i).turnNum = cnt;
+            playerList.get(i).setTurn(cnt);
             cnt++;
         }
     }
