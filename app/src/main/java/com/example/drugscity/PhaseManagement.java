b7b959b157046acfae68fb8a7b9481631da93b92
totem12 <7s1fma@gmail.com>
2020-09-12 16:31:21 +0900

    checkTurn関数の実装

--- app/src/main/java/com/example/drugscity/PhaseManagement.java
+++ app/src/main/java/com/example/drugscity/PhaseManagement.java
@@ -1,6 +1,7 @@
 package com.example.drugscity;
 
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PhaseManagement {
 
@@ -17,22 +18,31 @@ public class PhaseManagement {
         for(int i = 0; i < playerList.size(); i++) {
             //脱落したプレイヤーだったら飛ばす
             if(playerList.get(i).isDead()){
+                playerList.get(i).setTurn(0);
                 continue;
             }
-
             playerList.get(i).setTurn(cnt);
             cnt++;
         }
     }
 
-    void doAction(ArrayList<PlayerBase> playerList){
+    void checkTurn(ArrayList<PlayerBase> playerList){
         /***
-         引数のplayerListにセットされている順番通りに各自選んだアクションを実行する
+         引数のplayerListにセットされている順番を最終確認する
          ***/
-
+        //守るを選択したプレイヤーを探す
         for(int i = 0; i < playerList.size(); i++) {
-
-
+            //defenceは守るアクションの仮の名前
+            if(playerList.get(i).getAction == "defence"){
+                playerList.get(i).setTurn(0);
+            }
         }
+        //playerListをアクションの順番でソートする
+        Collections.sort(playerList, new Comparator<playerBase>(){
+            @Override
+            public int compare(playerList p1, playerList p2){
+                return p1.getTurn() - p2.getTurn();
+            }
+        });
     }
 }
