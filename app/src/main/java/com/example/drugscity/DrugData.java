b6a36b13720c00cb13bc430c652368ca20a11d7c
batacosan <batasan1998@gmail.com>
2020-09-19 21:08:18 +0900

    drugの名称と敗北条件を管理するデータクラスを作成

---
+++ app/src/main/java/com/example/drugscity/DrugData.java
@@ -0,0 +1,25 @@
+package com.example.drugscity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+private class DrugDataList {
+    //drugの名称
+    public static final String mazai = "魔剤";
+    public static final String grass = "草";
+    public static final String powderMint = "パウダーミント";
+    public static final String mouse = "マウス";
+    public static final String smokyAroma = "スモーキーアロマ";
+    public static final String joker = "ジョーカー";
+    public static final String spiderDrug = "スパイダードラッグ";
+    public static final String peridot = "ペリドット";
+    public static final String nShot = "nShot";
+    public static final String lifeBug = "ライフバグ";
+}
+
+public class DrugData {
+
+    //drugの名称
+    public String name;
+    public ArrayList<String> defeatCondition;
+}
