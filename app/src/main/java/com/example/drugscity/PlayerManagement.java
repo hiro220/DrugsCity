cf995ced57c9c187944ce288b3d130949e0627be
Ono111 <k_taro.1119@ezweb.ne.jp>
2020-10-03 22:14:31 +0900

    インスタンスの宣言

---
+++ app/src/main/java/com/example/drugscity/PlayerManagement.java
@@ -0,0 +1,26 @@
+package com.example.drugscity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerManagement extends AppCompatActivity {
+
+    //プレイヤーのインスタンス
+    player p1 = new player();
+
+    //CPUのインスタンス
+    cpu_player c1 = new cpu_player();
+    cpu_player c2 = new cpu_player();
+    cpu_player c3 = new cpu_player();
+    cpu_player c4 = new cpu_player();
+
+    //プレイヤーのアクション管理のインスタンス
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_management);
+    }
+
+
+}
