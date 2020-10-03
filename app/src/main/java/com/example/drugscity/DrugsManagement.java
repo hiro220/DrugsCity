2638cf9961bc17fd9fd5f5b8ef0775fea7743c4a
totem12 <7s1fma@gmail.com>
2020-10-03 18:53:10 +0900

    DrugsManagementにコメント追加

--- app/src/main/java/com/example/drugscity/DrugsManagement.java
+++ app/src/main/java/com/example/drugscity/DrugsManagement.java
@@ -3,10 +3,14 @@ package com.example.drugscity;
 
 public class DrugsManagement {
 
+    //選ばれた薬とその結果のリスト
     private String[][] choseList = new String[9][2];
+
+    //選ばれた薬のみのリスト
     private String[] choseDrugList = new String[9];
 
     void addChoseDrug(String addDrug, int i){
+        //選ばれた薬をリストに入れて覚えておく
         choseList[i][0] = addDrug;
         choseDrugList[i] = addDrug;
     }
