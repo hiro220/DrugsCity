209ea73fe698517994dd20c2f13f408223556d71
batacosan <batasan1998@gmail.com>
2020-10-03 23:04:00 +0900

    攻撃判定と敗北判定を行う関数judgeAttackを作成

--- app/src/main/java/DefeatCondition.java
+++ app/src/main/java/DefeatCondition.java
@@ -2,13 +2,25 @@ import java.util.Stack;
 
 public class DefeatCondition {
 
-    public Stack<String> drugOrigin;
+    public Stack<String> drugOrigin;   //敗北条件を元の状態に戻すためのデータ
     public DrugDataFrame drugStatus;
-    private Stack DrugdataBuf;
+    private Stack DrugdataBuf;         //一時的に敗北条件を退避させるためのバッファ
 
     DefeatCondition(DrugDataFrame drugData) {
         drugOrigin = drugData.defCondition;
         drugStatus = drugData;
     }
 
+    //攻撃が有効か判定し、敗北したかどうかを返す
+    public judgeAttack(String attackName) {
+        if(drugStatus.defCondition.peek() == attackName) {
+            drugStatus.pop();
+        }
+
+        if(drugStatus.empty) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
