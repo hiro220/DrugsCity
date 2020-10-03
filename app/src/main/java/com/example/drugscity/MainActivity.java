e8f8ab57b8846d440bd7e9885371c96df89472c6
hiroki-inoue-git <weeklyjump0928@gmail.com>
2020-10-03 22:27:34 +0900

    メインプロセスのインスタンス

--- app/src/main/java/com/example/drugscity/MainActivity.java
+++ app/src/main/java/com/example/drugscity/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainProcess app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ゲーム処理実行（あとでスレッド化する）
+        app = new MainProcess();
+        app.mainGameLoop();
     }
 }
