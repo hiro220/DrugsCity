4275b0913012be2561e361f1aba5c5bcdd2e4e88
totem12 <7s1fma@gmail.com>
2020-10-31 19:13:49 +0900

    intではなくStringで返すようにした

--- app/src/main/java/com/example/drugscity/RandomCpu.java
+++ app/src/main/java/com/example/drugscity/RandomCpu.java
@@ -3,28 +3,43 @@ package com.example.drugscity;
 import java.util.Random;
 
 public class CpuPlayer extends PlayerBase{
-    private int actionNumber;
-    private int drugNumber;
+    //配列の中身は仮
+    private String[] actionList = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
+    private String[] drugList = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
 
+    private String selectActionName;
+    private String selectDrugName;
+    private DrugsManagement drugsManagement = new DrugsManagement();
+
+    //ランダムで薬を選ぶ。一回選ばれた薬は選ばれない。
     void selectCpuDrug(){
-        drugNumber = randomInt();
+        while(true){
+            selectDrugName = drugList[randomInt()];
+            //選んだことのない薬だったらループから抜ける
+            if(!drugsManagement.isSearchDrug(selectDrugName)){
+                drugsManagement.addChoseDrug(selectDrugName);
+                break;
+            }
+        }
+
     }
 
+    //ランダムでアクションを選ぶ
     void selectCpuAction(){
-        actionNumber = randomInt();
+        selectActionName = actionList[randomInt()];
     }
 
-    int getDrugNumber(){
-        return drugNumber;
+    String getSelectDrugName(){
+        return selectDrugName;
     }
 
-    int getActionNumber(){
-        return actionNumber;
+    String getSelectActionName(){
+        return selectDrugName;
     }
 
-    //0~9の整数をランダムに返す
+    //0~8の整数をランダムに返す
     int randomInt() {
         Random rand = new Random();
-        return rand.nextInt(10);
+        return rand.nextInt(9);
     }
 }
